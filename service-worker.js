eeee5a02996375f2a2c2931338459c39d335c781
Shahrukh MdAdil <mdadils@amdocs.com>
2018-04-02 19:11:42 +0530

    added service workers

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// Listen for install event, set callback
+self.addEventListener('install', function(event) {
+    // Perform some task
+});
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it workss.',
+    icon: './src/images/icon.png',
+    badge: './src/images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
--- src/js/components/charts/TestChart.js
+++ src/js/components/charts/TestChart.js
@@ -1,51 +1,147 @@
 import React from "react";
 
+let isSubscribed = false;
+let swRegistration = null;
+let pushButton =  document.getElementById('registerMeButton');
+const applicationServerPublicKey = 'BPX35Ep7HKIFg3nEgZYovYiZuG8iJx1q2DCZ5FVbLzYN5xqtnm' +
+    'M_I1vexxLxUDPZsMao7EcoEkdZwegvBT_nep4';
 
 export default class TestChart extends React.Component {
 
   componentDidMount() {
-      var chart = new CanvasJS.Chart("chartContainer", {
-          title:{
-              text:"Fortune Global 500 Companies by Country"
-          },
-          animationEnabled: true,
-
-          data: [
-          {
-              type: "bar",
-              dataPoints: [
-                {y: 5, label: "Sweden"  },
-                {y: 6, label: "Taiwan"  },
-                {y: 7, label: "Russia"  },
-                {y: 8, label: "Spain"  },
-                {y: 8, label: "Brazil"  },
-                {y: 8, label: "India"  },
-                {y: 9, label: "Italy"  },
-                {y: 9, label: "Australia"  },
-                {y: 12, label: "Canada"  },
-                {y: 13, label: "South Korea"  },
-                {y: 13, label: "Netherlands"  },
-                {y: 15, label: "Switzerland"  },
-                {y: 28, label: "Britain" },
-                {y: 32, label: "Germany"   },
-                {y: 32, label: "France"  },
-                {y: 68, label: "Japan"   },
-                {y: 73, label: "China"},
-                {y: 132, label: "US" }
-              ]
-          }
-
-          ]
-      });
-      debugger;
-      console.log(chart);
-  chart.render();
+    pushButton =  document.getElementById('registerMeButton');
+    console.log('ComponentDIDMOunt');
 }
+
+registerSW(e) {
+  console.log('REGISTERING SW');
+  //alert('Hi ');
+  var self = this;
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(swReg) {
+      console.log('Service Worker is registered', swReg);
+      swRegistration = swReg;
+      self.initializeUI();
+    })
+    .catch(function(error) {
+      console.error('Service Worker Error', error);
+    });
+  } else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
+  }
+}
+
+initializeUI(e) {
+  var self = this;
+  pushButton.disabled = true;
+  if (isSubscribed) {
+    self.unsubscribeUser();
+  } else {
+    self.subscribeUser();
+  }
+
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    self.updateBtnText();
+  });
+}
+
+subscribeUser(e) {
+  const applicationServerKey = this.urlB64ToUint8Array(applicationServerPublicKey);
+  var self = this;
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+  .then(function(subscription) {
+    console.log('User is subscribed.');
+
+    self.updateSubscriptionOnServer(subscription);
+    self.isSubscribed = true;
+    self.updateBtnText();
+  })
+  .catch(function(err) {
+    console.log('Failed to subscribe the user: ', err);
+    self.updateBtnText();
+  });
+}
+
+unsubscribeUser() {
+  var self = this;
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    self.updateSubscriptionOnServer(null);
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+    self.updateBtnText();
+  });
+}
+
+urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails = document.querySelector('.js-subscription-details');
+
+  if (subscription && subscriptionJson && subscriptionDetails) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+updateBtnText() {
+  if (isSubscribed && pushButton) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+  pushButton.disabled = false;
+}
+
   render() {
     return (
       <div class="testChart">
-        <h1 class="welcomeScetionPlaceHolder">TEST CHART</h1>
-        <div id="chartContainer"></div>
+        <button type="button" id="registerMeButton" class="btn" onClick={this.registerSW.bind(this)}>Click Me</button>
+        <section id="js-subscription-details" class="js-subscription-details"></section>
+        <pre><code id="js-subscription-json" class="js-subscription-json"></code></pre>
+
       </div>
     );
   }
